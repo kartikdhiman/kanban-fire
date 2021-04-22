a96ade1adfa73542ff2b41226f717bbcb322a193
Kartik Dhiman <kartikjuly@gmail.com>
2021-04-22 19:34:36 +0530

    chore: production env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBrpxBiLfxijkSHnbNYiKro8qcdVKuZ_dw",
+    authDomain: "kanbanfire-e7f76.firebaseapp.com",
+    projectId: "kanbanfire-e7f76",
+    storageBucket: "kanbanfire-e7f76.appspot.com",
+    messagingSenderId: "23034723608",
+    appId: "1:23034723608:web:0383523362d2e0d63bb26d",
+    measurementId: "G-X9JTQT8CY1"
+  }
 };
