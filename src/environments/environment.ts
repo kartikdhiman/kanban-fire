c5a239f84a728462d3c0f526553c4b25da242eff
Kartik Dhiman <kartikjuly@gmail.com>
2021-04-22 08:09:06 +0530

    chore: configure firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBrpxBiLfxijkSHnbNYiKro8qcdVKuZ_dw",
+    authDomain: "kanbanfire-e7f76.firebaseapp.com",
+    projectId: "kanbanfire-e7f76",
+    storageBucket: "kanbanfire-e7f76.appspot.com",
+    messagingSenderId: "23034723608",
+    appId: "1:23034723608:web:0383523362d2e0d63bb26d",
+    measurementId: "G-X9JTQT8CY1"
+  }
 };
 
 /*
